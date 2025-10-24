d116442bee1d5b9106a154906dcbeeb11bc66792
IgorCielniak <igorcielniak.contact@gmail.com>
2025-10-24 11:44:58 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/repos/pryzma')
+def pryzma_repo():
+    return render_template('repos/pryzma.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
